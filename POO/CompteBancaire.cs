5e663de6ed7de8fd6905d732e272d033e98eb4fb
Cyril <cseguenot@gmail.com>
2023-03-07 10:29:07 +0100

    Fin du cours sur les constructeurs et initialiseurs

---
+++ POO/CompteBancaire.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+	internal class CompteBancaire
+	{
+		private const decimal PLAFOND_MAXI = 1000m;
+		private static decimal _tauxAgios;
+
+		#region Propriétés publiques
+		public required long Numero { get; set; }
+		public DateOnly DateCreation { get; set; }
+		public string Libelle { get; set; } = string.Empty;
+		public decimal Solde { get; private set; }
+		#endregion
+
+		#region Constructeurs
+		public CompteBancaire()
+		{
+		}
+
+		[SetsRequiredMembers]
+		public CompteBancaire(long numero)
+		{
+			Numero = numero;
+			DateCreation = DateOnly.FromDateTime(DateTime.Today);
+			Libelle = $"Compte N°{numero}";
+		}
+
+		[SetsRequiredMembers]
+		public CompteBancaire(long numero, DateOnly dateCreation) : this(numero)
+		{
+			DateCreation = dateCreation;
+		}
+
+		[SetsRequiredMembers]
+		public CompteBancaire(long numero, DateOnly dateCreation, string libelle) :
+			this(numero, dateCreation)
+		{
+			Libelle = libelle;
+		}
+		#endregion
+
+		#region Méthodes publiques (accessibles à l’extérieur de la classe)
+		public void Crediter(decimal montant)
+		{
+			Solde += montant;
+		}
+
+		// Méthode non statique
+		public void Debiter(decimal montant)
+		{
+			Solde -= montant;
+			if (Solde < 0)
+				Solde -= Solde * _tauxAgios; // Utilisation du champ statique OK
+		}
+
+		public void Debiter(decimal montant, string libelle)
+		{
+			Debiter(montant);
+			// ...code supplémentaire gérant le libellé
+		}
+
+		public void Debiter(decimal montant, string libelle, DateOnly dateEffet)
+		{
+			Debiter(montant, libelle);
+			// ...code supplémentaire gérant la date d'effet
+		}
+
+		// Méthode statique
+		public static void ModifierTauxAgios(decimal taux)
+		{
+			_tauxAgios = taux; // accès au champ statique OK
+			//Libelle = "COMPTE"; // accès à la propriété non statique pas OK
+		}
+		#endregion
+
+		protected virtual decimal CalculerInterets()
+		{
+			decimal interets = Solde * 0.005m;
+			Solde += interets;
+			return interets;
+		}
+	}
+
+	public class Client
+	{
+		public required string Nom { get; set; } = string.Empty;
+		public string Prenom { get; set; } = string.Empty;
+
+		public string NomComplet => $"{Nom} {Prenom}";
+
+		[SetsRequiredMembers]
+		public Client(string nom)
+		{
+			Nom = nom;
+		}
+	}
+}
---
+++ POO/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+
+
+namespace POO
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.Unicode;
+
+			// Instancie un compte bancaire et initialise son numéro
+			CompteBancaire compte1 = new CompteBancaire(123456);
+			CompteBancaire compte2 = new CompteBancaire(7556951);
+			Console.WriteLine(compte1.Numero);
+
+			// Utilisation de membres publics
+			compte1.Libelle = "Compte d'épargne";
+			compte1.Crediter(500);
+			compte1.Debiter(123, "achat d'un truc", new DateOnly(2025, 12, 25));
+			Console.WriteLine($"Le solde du compte {compte1.Libelle} est de {compte1.Solde:C2}");
+
+			CompteBancaire cpt3 = new CompteBancaire(8563214);
+			// Initialisation des propriétés en lecture / écriture sans constucteur, ni initialiseur
+			cpt3.DateCreation = new DateOnly(2030, 10, 15);
+			cpt3.Libelle = "Compte courant";
+
+			// Constructeur avec 3 paramètres
+			CompteBancaire cpt4 = new CompteBancaire(8563214, new DateOnly(2030, 10, 15), "azerty");
+
+			// Initialiseur
+			CompteBancaire cpt5 = new CompteBancaire {
+				Numero = 456465,
+				DateCreation = new DateOnly(2030, 10, 15),
+				Libelle = "dsfsdf"
+			};
+
+		}
+	}
+}
